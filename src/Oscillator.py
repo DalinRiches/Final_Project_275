f67fc95a2a78daf6fc5187b6ea76a2929f9ba429
Dalin Riches <dalinriches@gmail.com>
2017-03-24 17:12:18 -0600

    Added what type of oscilator we are making

--- src/Oscillator.py
+++ src/Oscillator.py
@@ -1,10 +1,21 @@
 class oscillator:
 
-    def __init__():
-        self.wave_table = []
-        self.wave_table_pos
-        self.voices
-        self.sync_pos
-        self.level
+    def __init__(self, wave_table="Triangle", wave_table_pos=0, voices=1, sync_pos=0, level=75):
+        self.wave_table = wave_table
+        self.wave_table_pos = wave_table_pos
+        self.voices = voices
+        self.sync_pos = sync_pos
+        self.level = level
 
-        hghg
+        self.generate_next_bit()
+
+    def generate_next_bit(self):
+        if self.wave_table_pos < 255:
+            self.wave_table_pos += 1
+        else:
+            self.wave_table_pos = 0
+        return self.wave_table[self.wave_table_pos]
+
+    def
+
+    # TODO: Make a proper  wave_table oscillator class
