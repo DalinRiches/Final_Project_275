d1bdc9f46e1c04bfa21114546a1c567e8f8f96d9
Dalin Riches <driches@ualberta.ca>
2017-04-07 16:47:12 -0600

    Working on the LFO's

--- src/client/Synth/LFO.py
+++ src/client/Synth/LFO.py
@@ -1,4 +1,12 @@
 class lfo:
 
-    def __init__(self, modControl, speed=1, amount=0.75, wavetype='sin'):
+    def __init__(self, modControl=None, speed=1, amount=0.75, wavetype='sin'):
+
+        self.modControl = modControl
+        self.speed = speed
+        self.amount = amount
+        self.wavetype = wavetype
+
+
+    def genOutput(self):
         pass
